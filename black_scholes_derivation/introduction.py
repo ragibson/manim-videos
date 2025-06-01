c3d391a2f07f3838452452ecc4b5cb4ca9423945
Ryan Gibson <ryan.alexander.gibson@gmail.com>
2025-05-31 20:58:19 -0400

    Refactor introduction script to have a bit more encapsulation

--- black_scholes_derivation/introduction.py
+++ black_scholes_derivation/introduction.py
@@ -6,13 +6,15 @@ from data import OCC_options_ADV  # noqa
 # TODO: also want "title card" style animation at the beginning
 
 class BlackScholesIntroduction(Scene):
-    def slide1(self):
+    def play_BSM_title(self):
         title = Text("1973: Black-Scholes-Merton Formula", font_size=48, t2c={"1973": YELLOW})
         title.to_edge(UP, buff=0.5)
 
         self.play(Write(title))
-        self.wait(1)
+        self.wait(1.0)
+        return title  # need to FadeOut later
 
+    def play_author_images(self):
         # black, scholes, merton images
         black_img = ImageMobject("licensed_images/Fischer_Black_cropped.jpg")
         scholes_img = ImageMobject("licensed_images/Myron_Scholes_2008_in_Lindau.jpg")
@@ -36,16 +38,32 @@ class BlackScholesIntroduction(Scene):
             self.play(FadeIn(images[i]), FadeIn(labels[i]))
             self.wait(0.5)
 
-        self.play(FadeOut(images), FadeOut(labels), FadeOut(title))
+        self.play(FadeOut(images), FadeOut(labels))
 
-    def slide2(self):
+    def play_nobel_prize(self):
+        # adding in image of the Nobel Prize medal
+        nobel_image = ImageMobject("licensed_images/Nobel_Prize.png")
+        nobel_image.height = 3.0
+
+        self.play(FadeIn(nobel_image, run_time=0.5))
+        self.wait(1.0)
+        return nobel_image  # going to want to shift this while OCC volume plot is being drawn
+
+    def play_OCC_volume_plot(self, title, nobel_image):
         # setting up axes and plot for OCC options daily volume
         axes = Axes(
             x_range=[1973, 2025, 4], y_range=[0, 60_000_000, 10_000_000],
-            x_length=10, y_length=6, tips=False,
+            x_length=10, y_length=4.5, tips=False,
             axis_config={"include_numbers": True, "font_size": 24},
-            x_axis_config={"include_numbers": [1973, 1975, 1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020, 2025]}
+            x_axis_config={
+                "include_numbers": [1973, 1975, 1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020, 2025],
+                "decimal_number_config": {
+                    "group_with_commas": False,  # these are years, so no commas
+                    "num_decimal_places": 0
+                }
+            }
         )
+        axes.next_to(title, DOWN, buff=1.0)
         x_label, y_label = axes.get_axis_labels(x_label=r"\text{Year}", y_label=r"\text{Average Daily Trading Volume}")
 
         graph = axes.plot_line_graph(
@@ -54,20 +72,18 @@ class BlackScholesIntroduction(Scene):
             add_vertex_dots=False, line_color=YELLOW, stroke_width=2
         )
 
-        self.play(Write(y_label))  # using this as kind of a slide header
-        self.play(Create(axes, run_time=4.0), Write(x_label))
-        self.play(Create(graph), run_time=4.0)
+        self.play(
+            Write(y_label),  # using this as kind of a slide header
+            nobel_image.animate.shift(LEFT * 1.0),
+            Create(axes, run_time=2.0), Write(x_label),
+            Create(graph, run_time=2.0)
+        )
         self.wait(2.0)
-
-        # adding in image of the Nobel Prize medal
-        nobel_image = ImageMobject("licensed_images/Nobel_Prize.png")
-        nobel_image.height = 3.0
-        nobel_image.shift(LEFT * 2.0 + UP * 1.0)
-
-        self.play(FadeIn(nobel_image))
-        self.wait(1.0)
+        return graph
 
     def construct(self):
-        # TODO: actually rename this functions into something meaningful
-        self.slide1()
-        self.slide2()
+        BSM_title = self.play_BSM_title()
+        self.play_author_images()
+        nobel_image = self.play_nobel_prize()
+        OCC_graph = self.play_OCC_volume_plot(BSM_title, nobel_image)
+        self.play(FadeOut(BSM_title), FadeOut(nobel_image), FadeOut(OCC_graph))
