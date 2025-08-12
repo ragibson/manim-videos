c8859cf411d3634cd61f9c96bf58ee77a90fda4c
Ryan Gibson <ryan.alexander.gibson@gmail.com>
2025-08-12 00:58:37 -0400

    Tweak render pipeline to use separate directories

--- black_scholes_derivation/batch_render.sh
+++ black_scholes_derivation/batch_render.sh
@@ -1,7 +1,7 @@
 # Rendering a batch in 480p15, 1080p60, and 2160p60 quality in parallel. The final high-quality render needs
 # Due to codec restrictions in DaVinci Resolve's Linux release, these will eventually need to be converted like so
 #   for i in *.mp4; do ffmpeg -i "$i" -vcodec dnxhd -profile:v dnxhr_lb -acodec pcm_s16le -f mov "${i%.*}.mov"; done
-(ls *.py | xargs -n 1 nice -n 19 manim -a -ql) &
-(sleep 1 && ls *.py | xargs -n 1 nice -n 19 manim -a) &
-(sleep 2 && ls *.py | xargs -n 1 nice -n 19 manim -a -qk) &
+(ls *.py | xargs -n 1 nice -n 19 manim -a -ql --media_dir media_ql) &
+(sleep 1 && ls *.py | xargs -n 1 nice -n 19 manim -a --media_dir media_qh) &
+(sleep 2 && ls *.py | xargs -n 1 nice -n 19 manim -a -qk --media_dir media_qk) &
 wait
